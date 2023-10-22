135daad9df9c9c1c3d95328bd4b39420c42366ba
Delight Sheriff <delightsheriff@gmail.com>
2023-10-22 21:34:28 +0100

    5. Number template-update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,7 +52,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     # Render the HTML template with n
-    return render_template('number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 # Run the app on 0.0.0.0, port 5000
